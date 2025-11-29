82dfe777ab0c6acfd34f3bcd334b2d8af7591d5e
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2025-11-29 18:43:52 +0100

    naprawione generowanie wspolrzednych deterministycznie

--- RMVB_konsola/RMVB_konsola/Generatory.cs
+++ RMVB_konsola/RMVB_konsola/Generatory.cs
@@ -14,37 +14,41 @@ namespace RMVB_konsola
         public static int liczba_urzadzen;
         private bool pierwszy = true;
 
-        private static int tmp = 0;
-        
+        private int stopnieNaSekundy(Decimal wejsciowa) {
+            int stopnie = ((int)wejsciowa / 1);
+            int minuty = (int)(wejsciowa % 1 / 0.01m);
+            int sekundy = (int)((wejsciowa % 0.01m) * 10000);
+            
+            return stopnie * 3600 + minuty * 60 + sekundy;
+        }
+
+        private Decimal sekundyNaStopnie(int wejsciowa) {
+            int stopnie = (int)(wejsciowa / 3600);
+            int minuty = (int)((wejsciowa - stopnie * 3600) / 60);
+            int sekundy = (int)(wejsciowa - minuty * 60 - stopnie * 3600);
+
+           return stopnie + minuty * 0.01m + sekundy * 0.0001m;
+
+        }
+
         //rozklad jednostajny (dyskretny?)
         public (Decimal, Decimal) generujWspolrzedneDeterministycznie()
         {
-            Console.WriteLine(tmp++);
             if (!pierwszy)
             {
                 //rozciaglosc poludnikowa to 5 st. 50 min.
                 //w minutach: 5 * 60 + 50 = 360
                 //w sekundach: 350 * 60 = 21000
+                int szerokosc_w_sekundach = stopnieNaSekundy(szerokosc);
+                szerokosc_w_sekundach += 21000 / liczba_urzadzen;
+                szerokosc = sekundyNaStopnie(szerokosc_w_sekundach);
 
-                //zrobic z tego fcje prywatna
-                int szerokosc_stopnie = ((int)szerokosc / 1);
-                int szerokosc_minuty = (int)(szerokosc % 1 / 0.01m);
-                int szerokosc_sekundy = (int)((szerokosc % 0.01m) * 10000);
-                int szerokosc_w_sekundach = szerokosc_stopnie * 3600 +
-                    szerokosc_minuty * 60 + szerokosc_sekundy;
-
-                szerokosc_w_sekundach += 210;
-
-                //zrobic z tego fcje prywatna
-                szerokosc_stopnie = (int)(szerokosc_w_sekundach / 3600);
-                szerokosc_minuty = (int)((szerokosc_w_sekundach - szerokosc_stopnie * 3600) / 60);
-                szerokosc_sekundy = (int)(szerokosc_w_sekundach - szerokosc_minuty * 60 - szerokosc_stopnie * 3600);
-
-                szerokosc = szerokosc_stopnie + szerokosc_minuty * 0.01m + szerokosc_sekundy * 0.0001m;
-
-                //poprawic dlugosc
-                dlugosc += Math.Round((10.00m / liczba_urzadzen), 4);
-                dlugosc += Math.Round((0.02m / liczba_urzadzen), 4);
+                //rozciaglosc rownoleznikowa to 10 st. 2 min.
+                //w minutach: 10 * 60 + 2 = 602
+                //w sekundach: 602 * 60 = 36120
+                int dlugosc_w_sekundach = stopnieNaSekundy(dlugosc);
+                dlugosc_w_sekundach += 36120 / liczba_urzadzen;
+                dlugosc = sekundyNaStopnie(dlugosc_w_sekundach);
             }
             pierwszy = false;
             return (dlugosc, szerokosc);
