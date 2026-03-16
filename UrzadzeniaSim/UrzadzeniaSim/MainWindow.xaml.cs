9a5a92bc589f3d5de1ea0a0c463e004e823d8933
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-03-16 20:35:20 +0100

    nauka WPF
    patrz notatki

---
+++ UrzadzeniaSim/UrzadzeniaSim/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace UrzadzeniaSim
+{
+    public partial class MainWindow : Window
+    {
+        bool klik = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            przycisk.Content = "Super!";
+            //przycisk.
+        }
+        private void przycisk_click(object sender, RoutedEventArgs e)
+        {
+            if (!klik)
+            {
+                tekst.Text = "yay";
+            }
+            else {
+                tekst.Text = "yippiee";
+            }
+            klik = !klik;
+        }
+    }
+    
+}
