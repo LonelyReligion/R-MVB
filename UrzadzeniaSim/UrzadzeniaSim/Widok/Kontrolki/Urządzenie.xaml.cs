f1faed35ce911f47f5ab27fdcc041d5cc96ceba3
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-04-09 13:50:51 +0200

    zaznaczanie urządzeń

--- UrzadzeniaSim/UrzadzeniaSim/Widok/Kontrolki/Urządzenie.xaml.cs
+++ UrzadzeniaSim/UrzadzeniaSim/Widok/Kontrolki/Urządzenie.xaml.cs
@@ -18,13 +18,16 @@ namespace UrzadzeniaSim.Widok.Kontrolki
     /// <summary>
     /// Logika interakcji dla klasy Urządzenie.xaml
     /// </summary>
-    public partial class Urządzenie : UserControl
+    public enum STATUS { 
+                NIEAKTYWNY,//wyszarzony?
+                AKTYWNY,
+                AKTYWNY_NADAJE
+    };
+
+public partial class Urządzenie : UserControl
     {
-        public enum STATUS { 
-            NIEAKTYWNY,//wyszarzony?
-            AKTYWNY,
-            AKTYWNY_NADAJE
-        };
+
+        bool zaznaczone = false;
 
         public decimal dlugosc;
         public decimal szerokosc;
@@ -33,6 +36,23 @@ namespace UrzadzeniaSim.Widok.Kontrolki
             InitializeComponent();
             this.dlugosc = dlugosc;
             this.szerokosc = szerokosc;
+        }
+
+        public void Zaznacz(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            var zaznaczenie = btn.Template.FindName("zaznaczenie", btn) as Ellipse;
+
+            zaznaczone = !zaznaczone;
+
+            if (zaznaczone)
+            {
+                zaznaczenie.Fill = new SolidColorBrush(Colors.Red);
+            }
+            else 
+            {
+                zaznaczenie.Fill = new SolidColorBrush(Colors.Transparent);
+            }
         }
     }
 }
