eef30bc008c168311b8873ae9a01de31ab9213a9
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-04-12 23:22:54 +0200

    dynamiczny rozmiar czcionki panelu bocznego

--- UrzadzeniaSim/UrzadzeniaSim/Widok/Kontrolki/PanelBoczny.xaml.cs
+++ UrzadzeniaSim/UrzadzeniaSim/Widok/Kontrolki/PanelBoczny.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -21,12 +22,55 @@ namespace UrzadzeniaSim.Widok.Kontrolki
     /// <summary>
     /// Logika interakcji dla klasy PanelBoczny.xaml
     /// </summary>
-    public partial class PanelBoczny : UserControl
+    public partial class PanelBoczny : UserControl, INotifyPropertyChanged
     {
         public event Action DodajUrzadzenie;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private double oryginalna_wysokosc;
+        private double oryginalna_szerokosc;
+
+        private const int bazowa_wielkosc_czcionki = 15;
+
+        private string wielkosc_czcionki = "15";
+        public string Wielkosc_czcionki 
+        {
+            get 
+            {
+                return wielkosc_czcionki;
+            }
+            set 
+            {
+                wielkosc_czcionki = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Wielkosc_czcionki"));
+            } 
+        }
+
+        private bool mamy_wymiary = false;
         public PanelBoczny()
         {
+            DataContext = this;
             InitializeComponent();
+            
+            panel.SizeChanged += (s,e) => 
+            {
+
+                double wysokosc_panelu = panel.ActualHeight;
+                double szerokosc_panelu = panel.ActualWidth;
+
+                if (!mamy_wymiary) {
+                    mamy_wymiary = true;
+                    oryginalna_wysokosc = wysokosc_panelu;
+                    oryginalna_szerokosc = szerokosc_panelu;
+                }
+
+                double skala_x = szerokosc_panelu / oryginalna_szerokosc;
+                double skala_y = wysokosc_panelu / oryginalna_wysokosc;
+                double skala = Math.Min(skala_x, skala_y);
+
+                Wielkosc_czcionki = (skala * (double)bazowa_wielkosc_czcionki).ToString();
+                            
+            };
         }
 
         public void ZlecGenerowanie(object sender, RoutedEventArgs e) {
