8b9959c386e64469cdc092971bcfb3b0f8603ab6
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-05-08 20:42:53 +0200

    wlasne wartosci przyblizenia

--- UrzadzeniaSim/UrzadzeniaSim/Widok/Kontrolki/PasekNarzedzi.xaml.cs
+++ UrzadzeniaSim/UrzadzeniaSim/Widok/Kontrolki/PasekNarzedzi.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows.Controls;
 using UrzadzeniaSim.Model.DB;
 using UrzadzeniaSim.Widok.Okna_dialogowe;
 using UrzadzeniaSim.Widok.Okna;
+using System.Xml.Linq;
+using System.ComponentModel;
 
 namespace UrzadzeniaSim.Widok.Kontrolki
 {
@@ -11,7 +13,7 @@ namespace UrzadzeniaSim.Widok.Kontrolki
     /// <summary>
     /// Logika interakcji dla klasy PasekNarzedzi.xaml
     /// </summary>
-    public partial class PasekNarzedzi : UserControl
+    public partial class PasekNarzedzi : UserControl, INotifyPropertyChanged
     {
         public double powiekszenie;
 
@@ -22,32 +24,38 @@ namespace UrzadzeniaSim.Widok.Kontrolki
         public event Action wyczysc_wszystko;
         public event Action dodaj_losowe;
         public event Action<(decimal, decimal)> dodaj_urzadzenie;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public Window rodzic;
         public Repo repo;
+        
+        private List<string> _domyslne_powiekszenia = new List<string> { "100%", "150%", "250%", "500%", "1000%" };
+        public List<string> domyslne_powiekszenia {
+            get {
+                return _domyslne_powiekszenia;
+            }
+            set {
+                _domyslne_powiekszenia = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("domyslne_powiekszenia"));
+            }
+        }
         public PasekNarzedzi()
         {
+            DataContext = this;
             InitializeComponent();
             
-            Powiekszenie.ItemsSource = new List<string> { "100%", "150%", "250%", "500%", "1000%" };
             Powiekszenie.SelectedIndex = 0;
         }
 
         private void zmianaPowiekszenia(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+            
             string procenty = Powiekszenie.SelectedItem?.ToString();
             int procenty_i;
-            if (procenty != null)
-            {
-                procenty_i = int.Parse(procenty.Substring(0, procenty.Length - 1));
-            }
-            else if (!string.IsNullOrEmpty(Powiekszenie.Text))
-            {
-                procenty_i = int.Parse(Powiekszenie.Text.Substring(0, Powiekszenie.Text.Length - 1)); //zakladajac ze konczy sie na % do poprawy
-            }
-            else {
+            if (procenty == null)
                 return;
-            }
 
+            procenty_i = int.Parse(procenty.Substring(0, procenty.Length - 1));
+        
             Trace.WriteLine("Zmieniono powiekszenie na " + procenty_i);
             powiekszenie = (double)procenty_i / 100;
 
@@ -105,6 +113,16 @@ namespace UrzadzeniaSim.Widok.Kontrolki
         {
             Ustawienia okno = new Ustawienia();
             okno.Show();
+        }
+
+        private void Powiekszenie_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var comboBox = (ComboBox)sender;
+            if (comboBox.SelectedItem != null)
+                return;
+            var newItem = comboBox.Text;
+            domyslne_powiekszenia.Add(newItem);
+            comboBox.SelectedItem = newItem;
         }
     }
 }
