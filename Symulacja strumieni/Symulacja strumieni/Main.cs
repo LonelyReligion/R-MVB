e5af29244ae5cffa9faa68a3a53b73bcc3ab3e83
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-06-11 14:54:26 +0200

    szkic

---
+++ Symulacja strumieni/Symulacja strumieni/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symulacja_strumieni
+{
+    internal class main
+    {
+        public static void Main()
+        {
+            Console.WriteLine("dzialam");
+        }
+    }
+}
