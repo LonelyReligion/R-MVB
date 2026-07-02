c57d5594e3bb8560156d2b86ae538ef4ee3915ad
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-07-02 22:35:21 +0200

    projekt wizualny, ale narazie nie sprawdzony w apce

--- UrzadzeniaSImScottplot/UrzadzeniaSImScottplot/okna/wyszukaj_srednia.xaml.cs
+++ UrzadzeniaSImScottplot/UrzadzeniaSImScottplot/okna/wyszukaj_srednia.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace UrzadzeniaSImScottplot.okna
+namespace UrzadzeniaSImScottplot
 {
     /// <summary>
     /// Logika interakcji dla klasy wyszukaj_srednia.xaml
