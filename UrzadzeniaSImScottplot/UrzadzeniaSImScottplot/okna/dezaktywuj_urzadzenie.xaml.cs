4df04a80fe224adb25a3b20078d0feef8c51cee2
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-08-10 18:46:53 +0200

    poczatek proj graficznego

---
+++ UrzadzeniaSImScottplot/UrzadzeniaSImScottplot/okna/dezaktywuj_urzadzenie.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UrzadzeniaSImScottplot.okna
+{
+    /// <summary>
+    /// Logika interakcji dla klasy dezaktywuj_urzadzenie.xaml
+    /// </summary>
+    public partial class dezaktywuj_urzadzenie : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        private int? _maxId;
+        public int? maxId
+        {
+            get { return _maxId; }
+            set
+            {
+                _maxId = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("maxId"));
+            }
+
+        }
+        private void _inicjujKontrolki()
+        {
+            using (var ctx = new Kontekst())
+            {
+                //maksymalne id urzadzenia
+                maxId = ctx.Urzadzenia.Max(u => (int?)u.UrzadzenieID);
+            }
+        }
+
+        public dezaktywuj_urzadzenie()
+        {
+            InitializeComponent();
+            _inicjujKontrolki();
+        }
+
+        private void IdUrzadzenia_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+        }
+    }
+}
