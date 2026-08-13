2067b99e2da9ef521128c286f497ebfc0501ce00
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-08-13 17:08:17 +0200

    zapisywalismy dubla

--- UrzadzeniaSImScottplot/UrzadzeniaSImScottplot/RMVB/RMVB.cs
+++ UrzadzeniaSImScottplot/UrzadzeniaSImScottplot/RMVB/RMVB.cs
@@ -50,7 +50,8 @@ namespace UrzadzeniaSImScottplot
         //usun
         internal void usunWersje(Wersja w)
         {
-            MVB.dodajUrzadzenie(w); //musi zostac zapisana najpierw
+            //MVB.szukaj(w.UrzadzenieID, w.WersjaID)
+            //MVB.dodajUrzadzenie(w); //musi zostac zapisana najpierw
             MVB.usunUrzadzenie(w); //jawnie dezaktywujemy urzadzenie, sprawdzamy czy nie nastpil weakVersionUnderflow
             repo.modifyVersion(w);
         }
