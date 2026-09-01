32d21ca625bae53c96a137cf4337bcbe96fa7430
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-09-01 22:25:31 +0200

    ujednolicanie wersji

--- PrezentacjaRownoleglosci/PrezentacjaRownoleglosci/rmvb/mvb/Korzen.cs
+++ PrezentacjaRownoleglosci/PrezentacjaRownoleglosci/rmvb/mvb/Korzen.cs
@@ -473,12 +473,12 @@ namespace Symulacja_strumieni.rmvb.mvb
             for (int i = wpisy.Count - 1; i >= 0; i--)
             {
                 var wpis = wpisy[i].Item2;
-                if (wpis.minData <= dt && wpis.maxData > dt && wpis.minKlucz <= id && wpis.maxKlucz >= id)
+                if ((wpis.minData <= dt && (wpis.maxData > dt || (wpis.maxData == DateTime.MaxValue && dt == DateTime.MaxValue))) && (wpis.minKlucz <= id && wpis.maxKlucz >= id))
                 {
                     for (int j = 0; j < wpis.wezel.urzadzenia.Count(); j++)
                     {
                         (int index, Wersja urzadzenie) = wpis.wezel.urzadzenia[j];
-                        if (index == id && urzadzenie.dataOstatniejModyfikacji <= dt && urzadzenie.dataWygasniecia > dt)
+                        if (index == id && urzadzenie.dataOstatniejModyfikacji <= dt && (urzadzenie.dataWygasniecia > dt || (dt == DateTime.MaxValue && urzadzenie.dataWygasniecia == DateTime.MaxValue)))
                             return urzadzenie;
                     }
                 }
