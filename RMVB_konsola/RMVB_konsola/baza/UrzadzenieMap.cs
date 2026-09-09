ddaa0538c05bb97eefd5e4ece97c84a53f270e71
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-09-09 18:07:44 +0200

    usuniecie niepotrzebnego pola

---
+++ RMVB_konsola/RMVB_konsola/baza/UrzadzenieMap.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMVB_konsola.baza
+{
+    public sealed class UrzadzenieMap : ClassMap<Urzadzenie>
+    {
+        public UrzadzenieMap() {
+            AutoMap(CultureInfo.InvariantCulture);
+            Map(m => m.rTimeAggregate).Ignore();
+        }
+    }
+}
