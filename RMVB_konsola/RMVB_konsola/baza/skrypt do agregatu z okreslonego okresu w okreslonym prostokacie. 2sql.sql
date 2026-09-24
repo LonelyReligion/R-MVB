80ab40999fc58fef266d0bfde4b3c4657688211d
Agnieszka Tażbirek <133567611+LonelyReligion@users.noreply.github.com>
2026-09-24 21:31:54 +0200

    w koncu 1:1

--- RMVB_konsola/RMVB_konsola/baza/skrypt do agregatu z okreslonego okresu w okreslonym prostokacie. 2sql.sql
+++ RMVB_konsola/RMVB_konsola/baza/skrypt do agregatu z okreslonego okresu w okreslonym prostokacie. 2sql.sql
@@ -1,20 +1,30 @@
-﻿SELECT AVG(Wartosc)
-FROM
-(
-	SELECT * 
-	FROM Urzadzenies AS U 
-	WHERE 14 <= U.Dlugosc 
-	AND   21 >= U.Dlugosc 
-	AND   50 <= U.Szerokosc 
-	AND   51 >= U.Szerokosc
-) AS U
+﻿DECLARE @poczatek datetime2(3) = '2026-09-24 20:34:38.262';
+DECLARE @koniec datetime2(3) = '2026-09-24 20:34:38.403';
 
-JOIN WersjaPomiars AS WP ON U.UrzadzenieID = WP.Wersja_UrzadzenieID
-JOIN Wersjas AS W ON W.WersjaID = WP.Wersja_WersjaID
+SELECT AVG(Wartosc)
+FROM
+	( 
+	SELECT U.UrzadzenieID, MAX(W.WersjaID)WersjaID
+	FROM
+	(
+		SELECT * 
+		FROM Urzadzenies AS U 
+		WHERE 15 <= U.Dlugosc 
+		AND   22 >= U.Dlugosc 
+		AND   49 <= U.Szerokosc 
+		AND   52 >= U.Szerokosc
+	) AS U
+	JOIN Wersjas AS W ON W.UrzadzenieID = U.UrzadzenieID
+	WHERE W.dataOstatniejModyfikacji >= @poczatek 
+	AND 
+	(
+	W.dataWygasniecia < @koniec OR 
+	(W.dataWygasniecia = '9999-12-31 23:59:59.9999999' AND @koniec = '9999-12-31 23:59:59.9999999'))
+	GROUP BY U.UrzadzenieID
+) AS W
+JOIN WersjaPomiars AS WP ON (W.UrzadzenieID = WP.Wersja_UrzadzenieID AND WP.Wersja_WersjaID = W.WersjaID)
 JOIN Pomiars AS P ON WP.Pomiar_PomiarID = P.PomiarID
 
-WHERE W.dataOstatniejModyfikacji >= '2026-09-23 17:02:49.351' 
-AND (W.dataWygasniecia < '2026-09-23 17:02:49.419' OR W.dataWygasniecia = '9999-12-31 23:59:59.9999999')
 
-AND P.dtpomiaru >= '2026-09-23 17:02:49.351' 
-AND P.dtpomiaru < '2026-09-23 17:02:49.419';
+AND P.dtpomiaru >= @poczatek
+AND P.dtpomiaru <  @koniec;
